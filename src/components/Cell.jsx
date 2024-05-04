5f6ee003dd9dc9e4a0aecc41773d061a1d4f9d17
Blazmian <demionfeick@gmail.com>
2024-05-03 19:07:45 -0700

    Added colors to numbers

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,11 +1,45 @@
 const Cell = ({ open, flagged, mine, adjacentMines, onClick, onRightClick }) => {
 
-    const cellClass = `h-8 w-8 transition-all duration-150 shadow-inner flex items-center justify-center shadow-gray-800  cursor-default ${flagged
+    const colorText = (adjacentMines) => {
+        let color = "";
+        switch (adjacentMines) {
+            case 1:
+                color = "text-blue-700";
+                break;
+            case 2:
+                color = "text-green-700";
+                break;
+            case 3:
+                color = "text-red-500";
+                break;
+            case 4:
+                color = "text-indigo-900";
+                break;
+            case 5:
+                color = "text-red-900";
+                break;
+            case 6:
+                color = "text-cyan-500";
+                break;
+            case 7:
+                color = "text-black";
+                break;
+            case 8:
+                color = "text-gray-500";
+                break;
+            default:
+                color = "text-black";
+                break;
+        }
+        return color;
+    };
+
+    const cellClass = `h-8 w-8 font-bold duration-150 shadow-inner flex items-center justify-center shadow-gray-800 cursor-default ${flagged
         ? "bg-gray-700"
         : open
             ? mine
                 ? 'bg-red-600'
-                : "bg-gray-400"
+                : `bg-gray-400 ${colorText(adjacentMines)}`
             : 'bg-gray-700 hover:shadow-gray-400 cursor-pointer'
         }`;
 
